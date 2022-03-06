4f2471e962b4583372d0f7facad6af5d50bf3504
BlaneTheKid <101032197+BlaneTheKid@users.noreply.github.com>
2022-03-06 17:32:00 -0600

    Update main.c
    HELLO SCHOOL

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("School\n");
-  }
-  return 0;
-}
+hello school 
+hello school 
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school 
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school 
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
+hello school
